6120e80013279f46e0c38c23c197174771b8e975
Niraj Kumar <nkr2348@gmail.com>
2024-06-28 00:25:09 +0530

    Defined some environment variables

---
+++ BlogApp/src/config/config.js
@@ -0,0 +1,13 @@
+// We follow this practice of storing environment variables like this because it makes sure that the values are always in String, it is important that those values are string otherwise it sometimes create problems in typescript , etc
+
+
+const config = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+
+}
+
+export default config
